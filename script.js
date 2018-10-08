4913cd87aa92791f9ae440ef8aea6246126eba78
StrixROX <pk1234pratyushk@gmailcom>
2018-10-08 23:26:20 +0530

    smooth scrolling enabled

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ function reset(){
     $('.topnav a').removeClass('active');
 }
 
+/* navbar update on click */
 $('body').ready(function(){
     $('.topnav a').click(function(){
         reset();
         $(this).toggleClass('active');
+        if(this.hash.slice(1) != ''){
+            name = '[name=' + this.hash.slice(1) + ']'
+            $('html, body').animate({scrollTop: $(name).offset().top}, 500);
+        }else{
+            $('html, body').animate({scrollTop: $('html').offset().top}, 500);
+        }
+
     });
 });
 
+/* navbar updater auto */
 $(window).scroll(function(){
     var i = document.getElementsByClassName('nav-item');
     var x = Math.round((window.pageYOffset/window.innerHeight));
